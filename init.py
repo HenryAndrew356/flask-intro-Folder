66090733823af98312dab1d6dda5bfa46dacebf7
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2023-06-12 18:59:55 -0500

    add info

---
+++ init.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+#   Creacion de Instancia de Flask ya que Flask es una clase
+app=Flask()
+
+
+
+#   Levantamiento de servidor
+#       En espera de posibles peticiones en tiempo indeterminador
+app.run()
