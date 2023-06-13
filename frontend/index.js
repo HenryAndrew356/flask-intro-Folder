2f7c2be9e1c57a96550992d57de891fc5f26e131
Henry Andrew <username@extension.com>
2023-06-13 16:45:35 -0500

    udpate

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,9 @@
 // const ROUTE_BACK="https://6487c4bd0e2469c038fc7b8a.mockapi.io"
-const ROUTE_BACK="http:127.0.0.1:5000"
+const ROUTE_BACK="http://127.0.0.1:5000"
 
-fetch(ROUTE_BACK+"/route_02",{method:"GET"})
+fetch(ROUTE_BACK+"/route_dictionaire",{method:"GET"})
     .then((respuesta)=>{
-        respuesta.json();   
+        return respuesta.json();
     })
     .then((data)=>{
         console.log(data);
@@ -11,3 +11,23 @@ fetch(ROUTE_BACK+"/route_02",{method:"GET"})
     .catch((error)=>{
         console.log(error);
     });
+
+fetch(ROUTE_BACK+"/products",{
+    method:"POST",
+    body:JSON.stringify({
+        name:"Zanahoria",
+        precio:1055.55
+    }),
+    headers:{
+        "Content-Type":"application/json",
+    }
+})
+    .then((respuesta)=>{
+        return respuesta.json();
+    })
+    .then((data)=>{
+        console.log(data)
+    })
+    .catch((error)=>{
+        console.error(error)
+    })
--- initial.py
+++ initial.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+'''
+    request
+        show the information solicitada by the client
+        Only its functional into of the ENDPOINT
+'''
 from datetime import datetime
 
+#   As it is microframework it is neccesary to install each library
+from flask_cors import CORS
 
 '''
 Creando la instancia de la clase Flask
@@ -11,6 +18,13 @@ __name__
 app=Flask(__name__)
 
 
+#   The class CORS, en caso se le asigne solamente las instancia de la clase Flask, entonces modificara los CORS para que pueden ser accedidos por todo el mundo, (cualquier origen, metodo, cabecera (header) )
+CORS(app)
+#   CORS(app=app)  <-  funciona igual
+
+
+#   make list for data of tuples
+products=[]
 
 
 '''
@@ -27,20 +41,38 @@ def initialRoute():
     return 'Bienvenido al endpoint inicial'
 
 
-@app.route('/route_02')
-def def_rout3_02():
+@app.route('/route_dictionaire')
+def def_route_dictionaire():
+    #  strftime : string for time
     return {
         'hour':datetime.now().strftime('%Y-%m-%d %H-%M-%S')
     }
 
-@app.route('/ruta_test')
+@app.route('/route_test')
 def def_route_test():
     return render_template('/frontend/index.html')
 
-'''
-Levantameinto del servidor
-    Se encuentra en espera de posibles peticiones en tiempo indeterminado
 
+@app.route('/products',methods=['POST'])
+def products_management():
+    #   .get_json()     <-      convert the Json que el cliente envia a un diccionario para que python lo pueda entender
+    print(request.get_json())
+    product=request.get_json()
+    products.append(product)
+    return {
+        'message':'Succesfully created product',
+        'content':product
+    }
+
+@app.route('/return_products',methods=['GET'])
+def list_of_producs():
+    return{
+        'message':'Los productos son:',
+        'content':products
+    }
+
+'''
+    Se encuentra en espera de posibles peticiones en tiempo indeterminado
 debug
     -   true : ante la deteccion de cambios en el codigo automaticamente se reinicia y 
         agrega los nuevos cambios
